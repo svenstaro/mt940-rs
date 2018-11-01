86d5defa992d05e8cf79fab2a2aeb953e4869c70
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-11-01 21:21:10 +0100

    WIP

---
+++ src/errors.rs
@@ -0,0 +1,103 @@
+use std::error;
+use std::fmt;
+
+use Rule;
+
+#[derive(Debug)]
+pub enum ParseError {
+    PestParseError(pest::error::Error<Rule>),
+    UnexpectedTagError(UnexpectedTagError),
+    RequiredTagNotFoundError(RequiredTagNotFoundError),
+}
+
+impl error::Error for ParseError {
+    fn source(&self) -> Option<&(error::Error + 'static)> {
+         match *self {
+             ParseError::PestParseError(ref err) => Some(err),
+             ParseError::UnexpectedTagError(ref err) => Some(err),
+             ParseError::RequiredTagNotFoundError(ref err) => Some(err),
+         }
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ParseError::PestParseError(ref err) => err.fmt(f),
+            ParseError::UnexpectedTagError(ref err) => err.fmt(f),
+            ParseError::RequiredTagNotFoundError(ref err) => err.fmt(f),
+        }
+    }
+}
+
+impl From<UnexpectedTagError> for ParseError {
+    fn from(err: UnexpectedTagError) -> ParseError {
+        ParseError::UnexpectedTagError(err)
+    }
+}
+
+impl From<RequiredTagNotFoundError> for ParseError {
+    fn from(err: RequiredTagNotFoundError) -> ParseError {
+        ParseError::RequiredTagNotFoundError(err)
+    }
+}
+
+#[derive(Debug)]
+pub struct UnexpectedTagError {
+    current_tag: String,
+    last_tag: String,
+    expected_tags: Vec<String>,
+}
+
+impl UnexpectedTagError {
+    pub fn new(
+        current_tag: String,
+        last_tag: String,
+        expected_tags: Vec<String>,
+    ) -> UnexpectedTagError {
+        UnexpectedTagError {
+            current_tag,
+            last_tag,
+            expected_tags,
+        }
+    }
+}
+
+impl error::Error for UnexpectedTagError {
+    fn source(&self) -> Option<&(error::Error + 'static)> {
+        None
+    }
+}
+
+impl fmt::Display for UnexpectedTagError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Unexpected tag '{}' found. Expected one of '{}'. The tag before this one was '{:?}.",
+            self.current_tag, self.last_tag, self.expected_tags
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct RequiredTagNotFoundError {
+    required_tag: String,
+}
+
+impl RequiredTagNotFoundError {
+    pub fn new(required_tag: String) -> RequiredTagNotFoundError {
+        RequiredTagNotFoundError { required_tag }
+    }
+}
+
+impl error::Error for RequiredTagNotFoundError {
+    fn source(&self) -> Option<&(error::Error + 'static)> {
+        None
+    }
+}
+
+impl fmt::Display for RequiredTagNotFoundError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Required tag '{}' not found.", self.required_tag,)
+    }
+}
